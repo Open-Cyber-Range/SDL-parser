f512f9375683912ec0f9771709071c113fd96299
Kaarel Allemann <kaarel.allemann@cr14.ee>
2022-02-23 16:11:26 +0200

    add parsing logic to go package

--- go-package/main.go
+++ go-package/main.go
@@ -2,13 +2,30 @@ package sdl_parser
 
 /*
 #cgo CFLAGS: -I/usr/include/sdl-parser
-#cgo LDFLAGS: -lsdl_parser
+#cgo LDFLAGS: -L/usr/lib/sdl-parser -lsdl_parser
 #include <stdlib.h>
 #include <sdl_parser.h>
 */
 import "C"
 
-func ParseSDL(sdlString string) string {
+import (
+	"encoding/json"
+	"errors"
+)
+
+func ParseSDL(sdlString string) (map[string]interface{}, error) {
 	cSDLString := C.CString(sdlString)
-	return "something"
+	responsePointer := C.parse_sdl_generate(cSDLString)
+	defer C.parse_sdl_free(responsePointer)
+
+	if responsePointer != nil {
+		responseString := C.GoString(responsePointer)
+		var response map[string]interface{}
+		json.Unmarshal([]byte(responseString), &response)
+		if response["status"] == "ERROR" {
+			return nil, errors.New(response["errorMessage"].(string))
+		}
+		return response["result"].(map[string]interface{}), nil
+	}
+	return nil, errors.New("failed to parse")
 }
