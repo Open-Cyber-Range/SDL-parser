9715f7a94c37b10da9a9d8e896d1b1e27d41c01f
Kaarel Allemann <kaarel.allemann@cr14.ee>
2022-02-23 13:46:30 +0200

    add header file support sdl_parser lib

---
+++ sdl-parser-export/build.rs
@@ -0,0 +1,32 @@
+extern crate cbindgen;
+
+use std::env;
+use std::path::{PathBuf};
+use cbindgen::Config;
+
+
+fn main() {
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    let package_name = "sdl_parser";
+    let output_file = target_dir()
+        .join(format!("{}.h", package_name))
+        .display()
+        .to_string();
+
+    let config = Config {
+        ..Default::default()
+    };
+
+    cbindgen::generate_with_config(&crate_dir, config)
+      .unwrap()
+      .write_to_file(&output_file);
+}
+
+fn target_dir() -> PathBuf {
+  let mut path_buffer = PathBuf::from(env::var("OUT_DIR").unwrap());
+  path_buffer.push("..");
+  path_buffer.push("..");
+  path_buffer.push("..");
+  path_buffer
+}
