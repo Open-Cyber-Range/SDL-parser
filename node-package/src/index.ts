ea728062c24027465f73431c8f4690b971d91d1f
Kaarel Allemann <kaarel.allemann@cr14.ee>
2022-02-28 16:44:33 +0200

    unify logic handling for node package

--- node-package/src/index.ts
+++ node-package/src/index.ts
@@ -5,11 +5,24 @@ const sdlLibrary = Library('libsdl_parser', {
   parse_sdl_free: ['void', ['char *']],
 });
 
-export default function parseSDL(sdlString: string): string {
+export default function parseSDL(sdlString: string): Object | null {
   const responsePointer = sdlLibrary.parse_sdl_generate(sdlString) as any;
+  let possibleErrorDuringExecution = null;
   try {
-    return responsePointer.readCString();
+    const responseString = responsePointer.readCString();
+    const response: any = JSON.parse(responseString);
+    if (response.status === 'SUCCESS') {
+      return response.result;
+    }
+    throw new Error(response.errorMessage);
+  } catch (e) {
+    possibleErrorDuringExecution = e;
   } finally {
     sdlLibrary.parse_sdl_free(responsePointer);
   }
+
+  if (possibleErrorDuringExecution) {
+    throw possibleErrorDuringExecution;
+  }
+  return null;
 }
