7503d1cf9fd83e74c46ae8368e1b981265dbd800
Madis-Mikk Remmet <madismikk.remmet@cr14.ee>
2022-03-15 16:06:58 +0200

    issue-6-fix

--- sdl-parser/src/lib.rs
+++ sdl-parser/src/lib.rs
@@ -1,25 +1,25 @@
 mod node;
-mod infrastructure;
 
 use anyhow::Result;
-use chrono::{ DateTime, Utc};
-use infrastructure::InfrastructureMap;
-use serde::{Serialize, Deserialize};
+use chrono::{DateTime, Utc};
+use node::{NodeMap};
+use serde::{Deserialize, Serialize};
 
 #[derive(PartialEq, Debug, Serialize, Deserialize)]
-pub struct Scenario { 
+pub struct Scenario {
     pub name: String,
+    pub description: Option<String>,
     pub start: DateTime<Utc>,
     pub end: DateTime<Utc>,
-    pub infrastructure: Option<InfrastructureMap>
+    pub infrastructure: Option<NodeMap>,
 }
 
 #[derive(PartialEq, Debug, Serialize, Deserialize)]
-pub struct Schema { 
+pub struct Schema {
     pub scenario: Scenario,
 }
 
-pub fn parse_sdl (sdl_string: &str) -> Result<Schema> {
+pub fn parse_sdl(sdl_string: &str) -> Result<Schema> {
     Ok(serde_yaml::from_str(sdl_string)?)
 }
 
@@ -39,55 +39,60 @@ mod tests {
         "#;
         let parsed_schema = super::parse_sdl(minimal_sdl).unwrap();
         let scenario_name = "test-scenario".to_string();
-        let start_time = DateTime::from(DateTime::parse_from_rfc3339("2022-01-20T13:00:00Z").unwrap());
-        let end_time = DateTime::from(DateTime::parse_from_rfc3339("2022-01-20T23:00:00Z").unwrap());
+        let start_time =
+            DateTime::from(DateTime::parse_from_rfc3339("2022-01-20T13:00:00Z").unwrap());
+        let end_time =
+            DateTime::from(DateTime::parse_from_rfc3339("2022-01-20T23:00:00Z").unwrap());
         let scenario = Scenario {
             name: scenario_name.clone(),
+            description: None,
             start: start_time,
             end: end_time,
-            infrastructure: None
+            infrastructure: None,
         };
-        let expected_schema = Schema {
-            scenario,
-        };
+        let expected_schema = Schema { scenario };
 
         assert_eq!(parsed_schema, expected_schema);
         assert_eq!(parsed_schema.scenario.name, scenario_name);
         assert_eq!(parsed_schema.scenario.start, start_time);
         assert_eq!(parsed_schema.scenario.end, end_time);
+        assert_eq!(parsed_schema.scenario.description, None);
     }
 
     #[test]
     fn includes_a_list_of_nodes() {
         let sdl = r#"
-            scenario:
-                name: test-scenario
-                start: 2022-01-20T13:00:00Z
-                end: 2022-01-20T23:00:00Z
-                infrastructure:
-                    test-infrastructure:
-                        description: some-test-description
-                        node:
-                            win10:
-                                type: VM
-                                template: windows10
-                                flavor:
-                                    ram: 4gb
-                                    cpu: 2
-                            deb10:
-                                type: VM
-                                template: debian10
-                                flavor:
-                                    ram: 2gb
-                                    cpu: 1
+        scenario:
+            name: test-scenario
+            description: some-description
+            start: 2022-01-20T13:00:00Z
+            end: 2022-01-20T23:00:00Z
+            infrastructure:
+                win10:
+                    type: VM
+                    description: win-10-description
+                    template: windows10
+                    flavor:
+                        ram: 4gb
+                        cpu: 2
+                deb10:
+                    type: VM
+                    description: deb-10-description
+                    template: debian10
+                    flavor:
+                        ram: 2gb
+                        cpu: 1
+
         "#;
         let parsed_schema = super::parse_sdl(sdl).unwrap();
 
         assert!(parsed_schema.scenario.infrastructure.is_some());
-        let infrastructure = parsed_schema.scenario.infrastructure.unwrap();
-        let node_map = infrastructure.get_key_value(
-            "test-infrastructure"
-        ).unwrap().1.to_owned().node.unwrap();
-        assert_eq!(node_map.values().len(), 2)
+        let node_map = parsed_schema.scenario.infrastructure.unwrap();
+        let node = node_map.get_key_value("win10").unwrap().1.to_owned();
+        assert_eq!(node_map.values().len(), 2);
+        assert_eq!(node.template, "windows10");
+        assert_eq!(node.flavor.ram, 4000000000);
+        assert_eq!(node.flavor.cpu, 2);
+        assert_eq!(node.description.unwrap(), "win-10-description".to_string());
     }
 }
