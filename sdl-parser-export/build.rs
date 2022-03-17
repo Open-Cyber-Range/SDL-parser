03fc5e04e1aed5d4c038c38248d67be1ae60a8b1
Madis-Mikk Remmet <madismikk.remmet@cr14.ee>
2022-03-17 09:24:54 +0200

    formatted all the existing *.rs files.

--- sdl-parser-export/build.rs
+++ sdl-parser-export/build.rs
@@ -1,33 +1,32 @@
 extern crate cbindgen;
 
+use cbindgen::{Config, Language::C};
 use std::env;
-use std::path::{PathBuf};
-use cbindgen::{Language::C, Config};
-
+use std::path::PathBuf;
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
     let package_name = "sdl_parser";
     let output_file = target_dir()
-      .join(format!("{}.h", package_name))
-      .display()
-      .to_string();
+        .join(format!("{}.h", package_name))
+        .display()
+        .to_string();
 
     let config = Config {
-      language: C,
-      ..Default::default()
+        language: C,
+        ..Default::default()
     };
 
     cbindgen::generate_with_config(&crate_dir, config)
-      .unwrap()
-      .write_to_file(&output_file);
+        .unwrap()
+        .write_to_file(&output_file);
 }
 
 fn target_dir() -> PathBuf {
-  let mut path_buffer = PathBuf::from(env::var("OUT_DIR").unwrap());
-  path_buffer.push("..");
-  path_buffer.push("..");
-  path_buffer.push("..");
-  path_buffer
+    let mut path_buffer = PathBuf::from(env::var("OUT_DIR").unwrap());
+    path_buffer.push("..");
+    path_buffer.push("..");
+    path_buffer.push("..");
+    path_buffer
 }
