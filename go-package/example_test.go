43b851c6af229c6385f3ac85497be0f3a7e8e012
Rait Rotšan <rait.rotsan@cr14.ee>
2022-04-01 15:23:40 +0300

    fixed indents

--- go-package/example_test.go
+++ go-package/example_test.go
@@ -5,12 +5,13 @@ import (
 )
 
 func ExampleParseSDL() {
-		fmt.Println(ParseSDL(`
+	
+	fmt.Println(ParseSDL(`
 scenario:
  name: test-scenario
  start: 2022-01-20T13:00:00Z
  end: 2022-01-20T23:00:00Z`))
-	
+ 
 	// Output:
 	// map[scenario:map[description:<nil> end:2022-01-20T23:00:00Z infrastructure:<nil> name:test-scenario start:2022-01-20T13:00:00Z]] <nil>
 
