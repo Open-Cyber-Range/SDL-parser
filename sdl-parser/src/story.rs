0117fcf8f1506ab7eb622903ab4a0d4f5d8eb014
Kristi Reispass <kristi.reispass@cr14.ee>
2023-01-24 11:12:13 +0200

    add default clock value and connection validation

--- sdl-parser/src/story.rs
+++ sdl-parser/src/story.rs
@@ -1,15 +1,28 @@
+use crate::helpers::Connection;
+use crate::Formalize;
+use crate::{constants::default_clock_value, script::Script};
 use anyhow::{anyhow, Result};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-use crate::Formalize;
-
-#[derive(PartialEq, Debug, Serialize, Deserialize, Clone)]
+#[derive(PartialEq, Eq, Debug, Serialize, Deserialize, Clone, Default)]
 pub struct Story {
-    #[serde(alias = "Clock", alias = "CLOCK")]
+    #[serde(default = "default_clock_value", alias = "Clock", alias = "CLOCK")]
     pub clock: u64,
     #[serde(alias = "Scripts", alias = "SCRIPTS")]
     pub scripts: Vec<String>,
+}
+
+impl Story {
+    pub fn new(potential_clock: Option<u64>) -> Self {
+        Self {
+            clock: match potential_clock {
+                Some(clock) => clock,
+                None => default_clock_value(),
+            },
+            ..Default::default()
+        }
+    }
 }
 
 pub type Stories = HashMap<String, Story>;
@@ -22,6 +35,28 @@ impl Formalize for Story {
 
         if self.clock < 1 {
             return Err(anyhow!("Clock value must be at least 1"));
+        }
+
+        Ok(())
+    }
+}
+
+impl Connection<Script> for (&String, &Story) {
+    fn validate_connections(&self, potential_script_names: &Option<Vec<String>>) -> Result<()> {
+        if potential_script_names.is_none() {
+            return Err(anyhow!(
+                "Story is defined but no Scripts declared under Scenario"
+            ));
+        };
+
+        if let Some(script_names) = potential_script_names {
+            for story_script_name in &self.1.scripts {
+                if !script_names.contains(story_script_name) {
+                    return Err(anyhow!(
+                        "Script {story_script_name} not found under Scenario"
+                    ));
+                }
+            }
         }
 
         Ok(())
