1370392025c3a6e39779d015e34e2c33888476cb
Kristi Reispass <kristi.reispass@cr14.ee>
2023-04-20 16:09:40 +0300

    change error jsvalue to string

--- wasm-sdl-parser/src/lib.rs
+++ wasm-sdl-parser/src/lib.rs
@@ -2,13 +2,13 @@ use sdl_parser::parse_sdl;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn parse_and_verify_sdl(sdl_string: &str) -> Result<String, JsValue> {
+pub fn parse_and_verify_sdl(sdl_string: &str) -> Result<String, String> {
     let result = parse_sdl(sdl_string);
     match result {
         Ok(scenario) => match serde_json::to_string(&scenario) {
             Ok(json) => Ok(json),
-            Err(err) => Err(JsValue::from_str(&err.to_string())),
+            Err(err) => Err(err.to_string()),
         },
-        Err(err) => Err(JsValue::from_str(&err.to_string())),
+        Err(err) => Err(err.to_string()),
     }
 }
